30c5ebf6f487d708f764db3ebc23d2da479d5af0
JSheleg <jessica.sheleg@gmail.com>
2022-02-21 17:44:43 -0600

    fix: update shop data for unique ids across all shops so cart util can group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import { connect} from 'react-redux';
 import './collection-item.style.scss';
+import CustomButton from '../custom-button/custom-bottom.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
 
+
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
+
+    return(
     <div className='collection-item'>
         <div 
             className='image'
-            style={{backgroundImage: `url(${imageUrl})`
+            style={{
+                backgroundImage: `url(${imageUrl})`
             }}
-            />
-
+        />
         <div className='collection-footer'>
             <span className='name'>{name} </span>
             <span className='price'>{price}</span>     
         </div>
+        <CustomButton onClick={() => addItem(item)} inverted>
+        Add To Cart
+        </CustomButton>
     </div>
-)
+)}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+//utilty functions in regards to cart redux code
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    if(existingCartItem){
+        return cartItems.map(cartItem => 
+        cartItem.id === cartItemToAdd.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem    
+        )
+    }
+
+    return [...cartItems, {...cartItemToAdd, quantity:1}]
+}
